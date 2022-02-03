e5f1e12aaba9ec70dcab8eb9a3f1a135c3894574
RohanPNaher <RohanPNaher@gmail.com>
2022-02-02 21:12:08 -0500

    removed unnecessary code

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ const btnSubmitElement = document.getElementById('submit-button')
 const ulElement = document.getElementById('todo-list')
 const btnResetElement = document.getElementById('reset-button')
 
-console.log(inpElement, btnSubmitElement, ulElement, btnResetElement)
 //Add to list
 btnSubmitElement.addEventListener('click', event => {
   let newListElement = document.createElement('li')
@@ -22,6 +21,4 @@ btnResetElement.addEventListener('click', event => {
 });
 
 //Clean
-ulElement.addEventListener('click', event => {
-  ulElement.removeChild(event.target)
-});
+ulElement.addEventListener('click', event => ulElement.removeChild(event.target));
