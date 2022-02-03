fb55ff4812139e04e423d3f7136a0fd284e2def5
RohanPNaher <RohanPNaher@gmail.com>
2022-02-03 17:48:59 -0500

    add form functionality learned from guess the number code along

--- js/app.js
+++ js/app.js
@@ -3,22 +3,43 @@ const inpElement = document.getElementById('my-input')
 const btnSubmitElement = document.getElementById('submit-button')
 const ulElement = document.getElementById('todo-list')
 const btnResetElement = document.getElementById('reset-button')
+const formElement = document.querySelector('form')
 
+
+
+
+//Event Listeners
 //Add to list
-btnSubmitElement.addEventListener('click', event => {
+formElement.addEventListener('submit', event => {
+  event.preventDefault()
+  addList()
+});
+
+//Reset
+formElement.addEventListener('reset', event => {
+  event.preventDefault()
+  init()
+});
+
+//Clean
+ulElement.addEventListener('click', event => ulElement.removeChild(event.target));
+
+//Functions
+//init
+init()
+
+function init() {
+  inpElement.value = ''
+  ulElement.innerHTML = ''
+  btnResetElement.setAttribute('hidden', true)
+}
+
+function addList () {
   let newListElement = document.createElement('li')
   newListElement.textContent = inpElement.value 
   if (inpElement.value !== '') {
     ulElement.appendChild(newListElement)
     inpElement.value = ''
   }
-});
-
-//Reset
-btnResetElement.addEventListener('click', event => {
-  let allListElements = document.querySelectorAll('li')
-  allListElements.forEach(arg => ulElement.removeChild(arg))
-});
-
-//Clean
-ulElement.addEventListener('click', event => ulElement.removeChild(event.target));
+  btnResetElement.removeAttribute('hidden')
+}
